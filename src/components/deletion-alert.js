92d18f75e37a9052b87417d8af2c47b555342d17
Emily Harber <emily.a.harber@gmail.com>
2020-07-03 17:59:43 -0400

    close button for deletion alert working

--- src/components/deletion-alert.js
+++ src/components/deletion-alert.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Alert, AlertTitle } from '@material-ui/lab';
 
-export default ({ puppy }) => {
+export default ({ puppy, closeAlert }) => {
   return (
-    <Alert color='warning' style={{ marginBottom: '1em' }}>
+    <Alert color='warning' style={{ marginBottom: '1em' }} onClose={closeAlert}>
       <AlertTitle>Goodbye, {puppy.name}!</AlertTitle>
       {puppy.name} has been deleted...
     </Alert>
--- src/components/main.js
+++ src/components/main.js
@@ -1,16 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Container, Grid } from '@material-ui/core';
 import PuppyCard from './puppy-card';
 import DeletedAlert from './deletion-alert';
 
 export default ({ puppies }) => {
+  let [showAlert, setShowAlert] = useState(false);
   let location = useLocation();
+
+  useEffect(() => {
+    if (location.state) {
+      setShowAlert(true);
+    }
+  }, [location]);
+
+  const closeAlert = () => {
+    setShowAlert(false);
+  };
 
   return (
     <Container maxWidth='lg' id='container'>
-      {location.state ? (
-        <DeletedAlert puppy={location.state.deletedPuppy} />
+      {showAlert ? (
+        <DeletedAlert
+          puppy={location.state.deletedPuppy}
+          closeAlert={closeAlert}
+        />
       ) : null}
       <Grid container spacing={3}>
         {puppies.map((puppy) => {
